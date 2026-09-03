ab5dcf38e0c6db6816713c752c60a7e6d5defdf2
khghouse <khghouse@naver.com>
2026-09-04 00:32:32 +0900

    feat: 할 일 목록 조회 API 추가

---
+++ internal/todo/todo.go
@@ -0,0 +1,10 @@
+package todo
+
+import "time"
+
+type Todo struct {
+	ID        string    `json:"id"`
+	Title     string    `json:"title"`
+	Completed bool      `json:"completed"`
+	CreatedAt time.Time `json:"createdAt"`
+}
